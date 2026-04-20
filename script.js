f229e8fa4f9d967a204840be7a4dc0735b3812c1
Samarth Shettigar <160016872+SamarthShettigar@users.noreply.github.com>
2026-04-20 17:00:06 +0530

    Added JS file

---
+++ script.js
@@ -0,0 +1,295 @@
+/*onst student={
+    fullName:"Samarth Shettigar",
+    age:22,
+    cgpa:7.6,
+    isPass:true
+
+};
+
+student.isPass=false;
+
+console.log(student.isPass);
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a % b = ",a%b);
+console.log("a ** b = ",a**b);
+console.log("a++ = ",a);
+console.log("a+=4 ",a+=4);
+console.log(a); */
+
+// let a = 5;
+// let b = 6;
+
+// let cond1 = a<b;
+// let cond2 = b===6;
+
+// console.log(cond1&&cond2);
+
+// let color;
+// if(mode === "dark-mode"){
+//     color = "black";
+// }
+
+
+// let age = 5;
+// if (age > 18){
+//     console.log("You Can Vote!");
+// }
+// else{
+//     console.log("Sorry");
+// }
+
+
+// let  num=10.2;
+// if(num%2 === 0){
+//     console.log("The Number is even")
+// }
+// else{
+//     console.log("The number is Odd")
+// }
+
+// let age = 40;
+// let result = age > 18 ? "adult" : "Not Adult";
+// console.log(result)
+
+// let name = prompt("Enter Your Name: ");
+// console.log("Hello, ",name);
+
+
+// let num = prompt("Enter the Number: ");
+// num = Number(num);
+
+// console.log("The number is: ",num);
+
+// if(num%5 === 0){
+//     console.log("It is Multiple of 5");
+// }
+// else{
+//     console.log("Not multiple");
+// }
+
+
+// let marks = prompt("Enter the marks: ");
+// marks=Number(marks);
+// if(marks>=90){
+//     console.log(" grade is A");
+// }
+// else if(marks<=89 && marks >=60){
+//     console.log("grade is B");
+// }
+// else if(marks<=59 && marks>=30){
+//     console.log("grade is C");
+// }
+// else if(marks<=29){
+//     console.log("grade is D")
+// }
+
+
+
+
+// let marks = 8;
+// let grade;
+
+// if(marks>=90){
+//     grade="A";
+// }
+// else if(marks<=89 && marks >=60){
+//     grade="B";
+// }
+// else if(marks<=59 && marks>=30){
+//     grade="C";
+// }
+// else if(marks<=29){
+//     grade="D";
+// }
+// console.log(grade)
+
+
+// let i;
+// for(i=1;i<=5000;i++){
+//     console.log("Hello")
+// }
+
+
+// let sum = 0;
+
+// for( let i=1 ; i<=5 ; i++){
+//     sum=sum+i;
+//     console.log(sum)
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log(i)
+// }
+
+// let i=1;
+
+// do{
+//     console.log("i = ",i);
+//     i++;
+// }
+// while(i<=5);
+
+// let str="Samarth";
+// let len=0;
+// for( let val of str){
+//     console.log("val = ",val)
+//     len++;
+// }
+
+
+// console.log(len);
+
+
+// let student={
+//     name: "Samarth",
+//     age:22,
+//     cgpa:7.6,
+//     isPass:true,
+
+// };
+// for(let i in student){
+//     console.log(i);
+// }
+
+// let i=0;
+// for( i=0;i<=100;i++){
+//     console.log("i = ",i);
+//     i++;
+// }
+
+// let str="Samarth";
+// str.length;
+// console.log(str.length);
+
+// let obj={
+//     item: "pen",
+//     price: 10,
+// };
+
+// let output = `The price of ${obj.item} is ${obj.price} rupees.`
+// console.log(output)
+
+// let fullName = prompt("Enter your name");
+// console.log(fullName);
+// let authName = "@" + fullName + fullName.length;
+// console.log("New Name is: ",authName);
+
+// let marks=[89,33,56,34,34];
+// console.log(marks);
+// for( let i=0;i<=marks.length;i++){
+//     console.log(marks[i])
+// }
+
+
+// let heroes =["ironman", "batman", "Superman", "thur", "hulk"];
+
+// for (hero of heroes){
+//     console.log(hero.toUpperCase());
+// }
+
+
+// let marks=[43,54,54,32,56,97];
+// let sum=0;
+
+// for( let val of marks){
+//     sum+=val;
+// }
+// console.log("The total marks of class: ",sum);
+
+// let avg=sum/marks.length;
+// console.log(`The avg marks of class is: ${avg}`);
+
+
+// let items = [200,300,450,600,250,800,900];
+// let i=0;
+// for (let val of items){
+// console.log(`value at index ${i} = ${val}`);
+// let offer = val/10;
+// items[i]=items[i]-offer;
+// console.log(` The price after offer is : ${items[i]}`);
+// i++;
+// }
+
+
+// let foodItems = ["Tomato", "potato", "Cucumber"];
+// foodItems.push("Hehehe");
+// foodItems.pop();
+// console.log(foodItems);
+
+// let companies = ["Google","Microsoft", "Oracle", "Amazon"];
+// companies.splice(2,1,"ola");
+// console.log(companies);
+
+// function myFunction(){
+//     console.log("Helloooooooooo Haawbeeebeeeeeee");
+//     console.log("asdfghjzxcvbnmzxcvbnm");
+// }
+// myFunction();
+// myFunction();
+// myFunction();
+
+
+// function myFunction(msg ,n){
+//     console.log(msg);
+// }
+// myFunction("Hehehehhahaha",80);
+
+
+
+// function sum(x,y){
+//     console.log(x+y);
+// }
+// sum(45,5);
+
+// const arrowSum=(a,b)=>{
+//     console.log(a+b);
+    
+// }
+
+// function countVowels(str){
+//     let count = 0;
+
+//     for(const char of str){
+//         if( char === "a" ||
+//             char === "e" ||
+//             char === "i" ||
+//             char === "o" ||
+//             char === "u"
+//         ){
+//             count ++;
+//         }
+//     }
+//     return count;
+// }
+
+
+// let array = ["Ram","Shyam","Laxman"];
+
+// array.forEach((val,idx,arr)=>{
+//     console.log(val.toUpperCase(),idx,array);
+// });
+
+
+// let array = [2,3,4,54,5,6];
+
+// array.forEach((val)=>{
+//     console.log(val*val);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
